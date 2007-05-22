efec797c9641d723b34dce419df326b8a759d1ff
Toru Yamaguchi <trymch+public@gmail.com>
2007-05-22 07:17:43 +0000

    Import of ZIGOROU/MozRepl-Plugin-Restart-0.02 from CPAN.
    gitpan-cpan-distribution: MozRepl-Plugin-Restart
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ZIGOROU/MozRepl-Plugin-Restart-0.02.tar.gz
    gitpan-cpan-author:       ZIGOROU
    gitpan-cpan-maturity:     released

--- lib/MozRepl/Plugin/Restart.pm
+++ lib/MozRepl/Plugin/Restart.pm
@@ -11,11 +11,11 @@ MozRepl::Plugin::Restart - Restart Firefox/Thunderbird
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
